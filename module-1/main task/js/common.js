d60e8e09440e0c6c16e18b746e469f43a0fb1a84
Andrii Khomenko <AndreW96Lyuda67@gmail.com>
2017-12-15 19:04:01 +0200

    wkn;l

--- module-1/main task/js/common.js
+++ module-1/main task/js/common.js
@@ -9,13 +9,13 @@ const lengthOfSecondLine = (secondLine.length);
 const lengthOfThirdLine = (thirdLine.length);
 
 let firstSymbolOfFirstLine = (firstLine.charAt(0));
-let lastSymbolOfFirstLine = (firstLine.charAt(11));
+let lastSymbolOfFirstLine = (firstLine.charAt(lengthOfFirstLine-1));
 
 let firstSymbolOfSecondLine = (secondLine.charAt(0));
-let lastSymbolOfSecondLine = (secondLine.charAt(10));
+let lastSymbolOfSecondLine = (secondLine.charAt(lengthOfSecondLine-1));
 
 let firstSymbolOfThirdLine = (thirdLine.charAt(0));
-let lastSymbolOfThirdLine = (thirdLine.charAt(9));
+let lastSymbolOfThirdLine = (thirdLine.charAt(lengthOfThirdLine-1));
 
 let positionOfSymbol1 = (firstLine.indexOf("["));
 let positionOfSymbol2 = (firstLine.indexOf("]"));
