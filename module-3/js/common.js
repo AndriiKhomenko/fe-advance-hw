900361c3e08a3898dd629f260f5f17b2a1173782
Andrii Khomenko <AndreW96Lyuda67@gmail.com>
2018-01-12 16:00:57 +0200

    module-3

---
+++ module-3/js/common.js
@@ -0,0 +1,16 @@
+let keyboard = [["q","w","e","r","t","y","u","i","o","p","[","]"],
+["a","s","d","f","g","h","j","k","l",";","'"],
+["z","x","c","v","b","n","m",",",".","/"]];
+
+let arr1 = [];
+let arr2 = [];
+let arr3 = [];
+
+arr1.push(keyboard[1][5],keyboard[0][2],keyboard[1][8],keyboard[1][8],keyboard[0][8]);
+console.log(arr1.join(''));
+
+arr2.push(keyboard[1][6],keyboard[1][0],keyboard[2][3],keyboard[1][0],keyboard[1][1],keyboard[2][2],keyboard[0][3],keyboard[0][7],keyboard[0][9],keyboard[0][4]);
+console.log(arr2.join(''));
+
+arr3.push(keyboard[0][4],keyboard[0][3],keyboard[1][0],keyboard[0][7],keyboard[2][5],keyboard[0][2],keyboard[0][3]);
+console.log(arr3.join(''));
