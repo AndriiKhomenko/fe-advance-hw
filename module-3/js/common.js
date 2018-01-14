2d47edeb2becec0e630122b6c22c514ef2173f88
Andrii Khomenko <AndreW96Lyuda67@gmail.com>
2018-01-14 16:14:24 +0200

    pravka module-3

--- module-3/js/common.js
+++ module-3/js/common.js
@@ -1,16 +1,22 @@
-let keyboard = [["q","w","e","r","t","y","u","i","o","p","[","]"],
-["a","s","d","f","g","h","j","k","l",";","'"],
-["z","x","c","v","b","n","m",",",".","/"]];
+const alphabet = "qwertyuiop[]asdfghjkl;'zxcvbnm,./";
 
-let arr1 = [];
-let arr2 = [];
-let arr3 = [];
+let arr1 = alphabet.slice(0,12);
+let arr2 = alphabet.slice(12,23);
+let arr3 = alphabet.slice(23,33);
 
-arr1.push(keyboard[1][5],keyboard[0][2],keyboard[1][8],keyboard[1][8],keyboard[0][8]);
-console.log(arr1.join(''));
+arr1 = arr1.split('');
+arr2 = arr2.split('');
+arr3 = arr3.split('');
 
-arr2.push(keyboard[1][6],keyboard[1][0],keyboard[2][3],keyboard[1][0],keyboard[1][1],keyboard[2][2],keyboard[0][3],keyboard[0][7],keyboard[0][9],keyboard[0][4]);
-console.log(arr2.join(''));
+let keyboard = [];
+keyboard.push(arr1);
+keyboard.push(arr2);
+keyboard.push(arr3);
 
-arr3.push(keyboard[0][4],keyboard[0][3],keyboard[1][0],keyboard[0][7],keyboard[2][5],keyboard[0][2],keyboard[0][3]);
-console.log(arr3.join(''));
+let h = keyboard[1][5] + keyboard[0][2] + keyboard[1][8] + keyboard[1][8] + keyboard[0][8];
+let j = keyboard[1][6] + keyboard[1][0] + keyboard[2][3] + keyboard[1][0] + keyboard[1][1] + keyboard[2][2] + keyboard[0][3] + keyboard[0][7] + keyboard[0][9] + keyboard[0][4];
+let t = keyboard[0][4] + keyboard[0][3] + keyboard[1][0] + keyboard[0][7] + keyboard[2][5] + keyboard[0][2] + keyboard[0][3];
+
+console.log(h);
+console.log(j);
+console.log(t);
