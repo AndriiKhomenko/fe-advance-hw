001711c26aaebacacf315de745f6238c2aa22db8
Andrii Khomenko <AndreW96Lyuda67@gmail.com>
2018-02-27 15:22:16 +0200

    swipe

--- fp/js/common.js
+++ fp/js/common.js
@@ -18,4 +18,26 @@ function myMove(e){
 			a.classList.add("move-left");
 	}
 }
-window.addEventListener("touchstart", myMove);
+window.addEventListener("click", myMove);
+
+window.addEventListener("load", function(){
+    swipearea = document.getElementById("cards")
+    swipearea.addEventListener('touchstart', function(e){
+        startX = e.changedTouches[0].clientX
+        startY = e.changedTouches[0].clientY
+    })
+    swipearea.addEventListener('touchend', function(e){
+        endX = e.changedTouches[0].clientX
+        endY = e.changedTouches[0].clientY
+        if (endX-startX>=30 && (Math.abs(endY-startY)<=200)){
+          if(a.classList.contains("move-right")){
+						a.classList.remove("move-right");
+						a.classList.add("move-left");
+					}
+					else{
+						a.classList.add("move-right");
+						a.classList.remove("move-left");
+					}
+        }
+    })
+})
