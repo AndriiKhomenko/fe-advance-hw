61440c245072e1fff04627c91e20d16bf4fa000c
Andrii Khomenko <AndreW96Lyuda67@gmail.com>
2018-02-27 20:59:52 +0200

    slvldks

--- fp/js/common.js
+++ fp/js/common.js
@@ -3,32 +3,31 @@
 	// 		a.classList.remove("move-right");
 	// 		a.classList.add("move-left");
 	// }
-window.addEventListener('load', function(){
+window.addEventListener("load", function(){
     let swipearea = document.getElementById("wrapper");
     let button = document.querySelector("#button");
-    swipearea.addEventListener('mousedown', function(e){
+    let a = document.querySelector(".menu");
+    swipearea.addEventListener("touchstart", function(e){
         startX = e.clientX;
         startY = e.clientY;
     }, false)
-    swipearea.addEventListener('mouseup', function(e){
+    swipearea.addEventListener("touchend", function(e){
         endX = e.clientX;
         endY = e.clientY;
-        let a = document.querySelector(".menu");
         if(endX-startX>0){
         	if(a.classList.contains("move-left")){
 						a.classList.remove("move-left");
         	}
         	a.classList.add("move-right");
         }
-        else if(endX-startX<0){
+        else if(endX-startX<=0){
         	if(a.classList.contains("move-right")){
         		a.classList.remove("move-right");
         	}
         a.classList.add("move-left");
       	}
     }, false)
-    button.addEventListener('click', function(e){
-        let a = document.querySelector(".menu");
+    button.addEventListener('touchstart', function(e){
 					if(a.classList.contains("move-right")){
 						a.classList.remove("move-right");
 						a.classList.add("move-left");
