9afa550b6ce33530627df430549474ba2258ba72
Andrii Khomenko <AndreW96Lyuda67@gmail.com>
2018-02-27 15:27:54 +0200

    svd

--- fp/js/common.js
+++ fp/js/common.js
@@ -1,24 +1,24 @@
-function myMove(e){
-	let a = document.querySelector(".menu");
-	let button = document.querySelector("#button");
-	let lines = document.querySelector(".line");
-	let card = document.querySelector("#cards");
-	if(e.target === button || lines){
-		if(a.classList.contains("move-right")){
-			a.classList.remove("move-right");
-			a.classList.add("move-left");
-		}
-		else{
-		a.classList.add("move-right");
-		a.classList.remove("move-left");
-		}
-	}
-	if(e.target === card){
-			a.classList.remove("move-right");
-			a.classList.add("move-left");
-	}
-}
-window.addEventListener("click", myMove);
+// function myMove(e){
+// 	let a = document.querySelector(".menu");
+// 	let button = document.querySelector("#button");
+// 	let lines = document.querySelector(".line");
+// 	let card = document.querySelector("#cards");
+// 	if(e.target === button || lines){
+// 		if(a.classList.contains("move-right")){
+// 			a.classList.remove("move-right");
+// 			a.classList.add("move-left");
+// 		}
+// 		else{
+// 		a.classList.add("move-right");
+// 		a.classList.remove("move-left");
+// 		}
+// 	}
+// 	if(e.target === card){
+// 			a.classList.remove("move-right");
+// 			a.classList.add("move-left");
+// 	}
+// }
+// window.addEventListener("click", myMove);
 
 window.addEventListener("load", function(){
     swipearea = document.getElementById("cards")
